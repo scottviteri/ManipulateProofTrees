7c976fd03b98a0bd697da5b37224d6665a60401c
Scott Viteri <scottviteri@gmail.com>
2019-08-14 13:48:17 -0400

    Added new theorems and rebuilt

--- ProofSourceFiles/isosceles_conga.v
+++ ProofSourceFiles/isosceles_conga.v
@@ -1,1 +1,6 @@
+Add LoadPath "/home/scottviteri/LocalSoftware/".
 Require Import GeoCoq.Highschool.triangles.
+
+Section T15.
+Context `{T2D:Tarski_2D}.
+Context `{TE:@Tarski_euclidean Tn TnEQD}.
---
+++ ProofSourceFiles/postulate_of_existence_of_a_triangle_whose_angles_sum_to_two_rights.v
@@ -0,0 +1,8 @@
+Add LoadPath "/home/scottviteri/LocalSoftware/".
+Require Import GeoCoq.Axioms.parallel_postulates.
+Require Import GeoCoq.Tarski_dev.Ch15_lengths.
+
+Section T15.
+Context `{T2D:Tarski_2D}.
+Context `{TE:@Tarski_euclidean Tn TnEQD}.
+
